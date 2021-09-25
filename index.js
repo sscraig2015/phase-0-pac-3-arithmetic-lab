e6495c8de39459ccd091af36da85a5855ce50554
Sean <craig.sean92@gmail.com>
2021-09-25 14:48:01 -0600

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,34 @@
+function add() {}
+function subtract(){}
+function multiply(){}
+function divide(){}
+function add(a, b) {
+    return a+b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a,b) {
+    return a*b;
+}
+function divide(a,b) {
+    return a/b;
+}
+function increment(n) {
+    n++;
+    return n; 
+}
+function decrement(n) {
+    n--;
+    return n;
+}
+function makeInt(n) {
+    
+    var result = parseInt(n, 10);
+    return result;
+
+}
+function preserveDecimal(n) {
+    var result = parseFloat(n);
+    return result;
+}
